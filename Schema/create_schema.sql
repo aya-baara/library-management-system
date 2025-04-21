7221f0d4080f1ae82e30bf82f1cd6353d19d1419
ayabaara <ayabaara4@gmail.com>
2025-04-21 17:34:09 +0300

    Create (Books,Borrowers,Loans) tables

---
+++ Schema/create_schema.sql
@@ -0,0 +1,32 @@
+create table Books(
+BookId int Primary Key,
+Title varchar(30) NOT NULL,
+Author  varchar(30) NOT NULL ,
+ISBN varchar(20) NOT NULL UNIQUE ,
+Published_Date Date NOT NULL,
+Genre  varchar(30) NOT NULL,
+Shelf_Location varchar(30) NOT NULL,
+Current_Status varchar(10) Check (Current_Status in ('Available','Borrowed')) NOT NULL
+);
+
+GO
+
+Create table Borrowers(
+BorrowerID int primary key ,
+First_Name varchar(50) NOT NULL ,
+Last_Name varchar(50) NOT NULL ,
+Email varchar(100) NOT NULL UNIQUE  ,
+Date_of_Birth Date Not NULL ,
+Membership_Date Date Not NULL 
+); 
+
+GO
+
+create table Loans(
+LoanID int primary key,
+BookID int NOT NULL foreign key references Books(BookId),
+BorrowerID int NOT NULL foreign key references Borrowers(BorrowerID),
+Date_Borrowed Date NOT NULL,
+Due_Date Date NOT NULL ,
+Date_Returned Date
+);
