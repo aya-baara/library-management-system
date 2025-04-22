820075e0622a01e59751daf3f1b59ad8942976a5
ayabaara <ayabaara4@gmail.com>
2025-04-22 09:59:39 +0300

    edit pk columns to be auto incremented

--- Schema/create_schema.sql
+++ Schema/create_schema.sql
@@ -1,5 +1,5 @@
 create table Books(
-BookId int Primary Key,
+BookId int Primary Key Identity (1,1),
 Title varchar(30) NOT NULL,
 Author  varchar(30) NOT NULL ,
 ISBN varchar(20) NOT NULL UNIQUE ,
@@ -12,7 +12,7 @@ Current_Status varchar(10) Check (Current_Status in ('Available','Borrowed')) NO
 GO
 
 Create table Borrowers(
-BorrowerID int primary key ,
+BorrowerID int primary key Identity (1,1),
 First_Name varchar(50) NOT NULL ,
 Last_Name varchar(50) NOT NULL ,
 Email varchar(100) NOT NULL UNIQUE  ,
@@ -23,7 +23,7 @@ Membership_Date Date Not NULL
 GO
 
 create table Loans(
-LoanID int primary key,
+LoanID int primary key Identity (1,1),
 BookID int NOT NULL foreign key references Books(BookId),
 BorrowerID int NOT NULL foreign key references Borrowers(BorrowerID),
 Date_Borrowed Date NOT NULL,
