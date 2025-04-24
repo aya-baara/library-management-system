3aaf00867314eb1d40b96a1dc7a81be57f86142a
ayabaara <ayabaara4@gmail.com>
2025-04-24 13:59:27 +0300

    refctor : return the query directly  instead of using varible

--- Queries/7.sql
+++ Queries/7.sql
@@ -4,11 +4,9 @@ create function fn_BookBorrowingFrequency(@bookId int)
 returns int
 as
 begin
-	declare @BookBorrowingFrequency int =0;
-	select @BookBorrowingFrequency= count(*) from loans l
-	where l.BookID=@bookId
+	
+	return (select count(*) from loans l where l.BookID=@bookId)
 
-	return @BookBorrowingFrequency;
 end
 
-SELECT dbo.fn_BookBorrowingFrequency(5) AS TimesBorrowed;
+SELECT dbo.fn_BookBorrowingFrequency(25) AS TimesBorrowed;
