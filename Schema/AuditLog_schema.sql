a3ec43273182de55a80e5ec64246eea6c395c3cc
ayabaara <ayabaara4@gmail.com>
2025-04-24 13:47:04 +0300

    Change ChangeDate column to NOT NULL datetime with default GETDATE()

--- Schema/AuditLog_schema.sql
+++ Schema/AuditLog_schema.sql
@@ -4,5 +4,5 @@ create table AuditLog (
 LogID INT PRIMARY KEY IDENTITY,
 BookId int NOT NULL foreign key references Books(BookId),
 StatusChange varchar (30),
-ChangeDate date
+ChangeDate datetime NOT NULL DEFAULT GETDATE()
 ) 
